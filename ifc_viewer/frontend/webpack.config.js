1decc5a5de2c6e793a9d3e4ab25d93386e40ce9c
Mostafa Gabr <124693861+mgabr2007@users.noreply.github.com>
2024-02-17 12:09:32 +0100

    Create webpack.config.js

---
+++ ifc_viewer/frontend/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM'
+  }
+};
